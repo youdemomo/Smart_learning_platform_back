a338b43f7dfd071883d64c59e6fb81cb8e34673e
Zhong Ting Qiang <2861315501@qq.com>
2025-11-26 14:41:56 +0800

    初始化项目

---
+++ src/main/java/com/zhihuixuexi/ZhihuixuexiApplication.java
@@ -0,0 +1,12 @@
+package com.zhihuixuexi;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ZhihuixuexiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ZhihuixuexiApplication.class, args);
+    }
+}
